21f5738c7ea2d358c32da557201c6d1ff6293187
albertolunia <albertolunia@gmail.com>
2024-08-13 13:48:28 -0300

    Migration inicial

--- src/Cepedi.ProjetoRFID.Data/EntityTypeConfiguration/ProductEntityTypeConfiguration.cs
+++ src/Cepedi.ProjetoRFID.Data/EntityTypeConfiguration/ProductEntityTypeConfiguration.cs
@@ -15,20 +15,24 @@ public class ProductEntityTypeConfiguration : IEntityTypeConfiguration<ProductEn
         builder.Property(e => e.IdTag).IsRequired();
         builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
         builder.Property(e => e.Description).IsRequired().HasMaxLength(100);
-        builder.Property(e => e.Weight).IsRequired();
+        builder.Property(e => e.Weight)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
         builder.Property(e => e.ManufacDate).IsRequired();
         builder.Property(e => e.DueDate).IsRequired();
         builder.Property(e => e.UnitMeasurement).IsRequired().HasMaxLength(20);
         builder.Property(e => e.PackingType).IsRequired().HasMaxLength(100);
         builder.Property(e => e.BatchNumber).IsRequired().HasMaxLength(100);
         builder.Property(e => e.Quantity).IsRequired();
-        builder.Property(e => e.Price).IsRequired();
+        builder.Property(e => e.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
 
         builder.HasOne(e => e.Category)
                 .WithMany()
                 .HasForeignKey(e => e.IdCategory)
                 .IsRequired();
-        
+
         builder.HasOne(e => e.Supplier)
                 .WithMany()
                 .HasForeignKey(e => e.IdSupplier)
