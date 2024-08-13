7b1b31632d014b179cfaf891da7e2f496970f9ef
albertolunia <albertolunia@gmail.com>
2024-08-13 15:10:00 -0300

    Hot fix adicionando a tag rfid ao product entity

--- src/Cepedi.ProjetoRFID.Domain/Entities/ProductEntity.cs
+++ src/Cepedi.ProjetoRFID.Domain/Entities/ProductEntity.cs
@@ -14,6 +14,7 @@ public class ProductEntity
     public int IdSupplier { get; set; }
     public SupplierEntity? Supplier { get; set; }
     public int IdTag { get; set; }
+    public RfidTagEntity? Tag { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Weight { get; set; }
