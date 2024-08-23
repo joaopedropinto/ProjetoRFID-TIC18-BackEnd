681b852e219ed14ea400298cf53529670da4a47a
Victor Quirino <victorsouzaq@gmail.com>
2024-08-23 11:00:45 -0300

    [#40]-Changing the ID type of entities to Guid

--- src/Cepedi.ProjetoRFID.Domain/Entities/RfidTagEntity.cs
+++ src/Cepedi.ProjetoRFID.Domain/Entities/RfidTagEntity.cs
@@ -2,7 +2,7 @@
 
 public class RfidTagEntity
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string RfidTag { get; set; }
 
     internal void Update(string rfidTag)
