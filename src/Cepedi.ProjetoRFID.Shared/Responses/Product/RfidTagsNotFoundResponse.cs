f5d520f2d3f3c5fe14535288e90dcf70bd743083
albertolunia <albertolunia@gmail.com>
2024-08-28 14:52:04 -0300

    Adicionando mais um campo no response

--- src/Cepedi.ProjetoRFID.Shared/Responses/Product/RfidTagsNotFoundResponse.cs
+++ src/Cepedi.ProjetoRFID.Shared/Responses/Product/RfidTagsNotFoundResponse.cs
@@ -1,9 +1,11 @@
 public record class RfidTagsNotFoundResponse
 {
+    public string RfidTag { get; init; }
     public string Message { get; init; }
 
     public RfidTagsNotFoundResponse(string rfidTag)
     {
+        RfidTag = rfidTag;
         Message = $"Produto não encontrado para a tag {rfidTag}";
     }
 }
