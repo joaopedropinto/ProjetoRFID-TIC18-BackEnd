093b332659545a813851884387a95679c1ba1ddd
Victor Quirino <victorsouzaq@gmail.com>
2024-09-10 17:02:03 -0300

    [#57]-Create Packaging entity and configurations

---
+++ src/Cepedi.ProjetoRFID.Domain/Entities/PackagingEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cepedi.ProjetoRFID.Domain.Entities
+{
+    public class PackagingEntity
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+
+        public void Delete()
+        {
+            IsActive = false;
+        }
+    }
+}
