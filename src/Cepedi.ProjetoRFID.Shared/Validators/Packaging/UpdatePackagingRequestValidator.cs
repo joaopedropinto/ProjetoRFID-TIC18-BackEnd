57ef4afb428dcfc89c7853e7a8408d61dcf54c9d
Victor Quirino <victorsouzaq@gmail.com>
2024-09-11 13:33:21 -0300

    [#57]-Add packaging request validators

---
+++ src/Cepedi.ProjetoRFID.Shared/Validators/Packaging/UpdatePackagingRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using Cepedi.ProjetoRFID.Shared.Requests.Packaging;
+using FluentValidation;
+
+namespace Cepedi.ProjetoRFID.Shared.Validators.Packaging
+{
+    public class UpdatePackagingRequestValidator : AbstractValidator<UpdatePackagingRequest>
+    {
+        public UpdatePackagingRequestValidator()
+        {
+            RuleFor(packaging => packaging.Id)
+                .NotEmpty().WithMessage("O id do tipo de embalagem deve ser informado");
+
+            RuleFor(packaging => packaging.Name)
+                .NotEmpty().WithMessage("O nome do tipo de embalegem deve ser informado")
+                .MinimumLength(3).WithMessage("O nome do tipo de embalegem deve ter no mínimo 3 caracteres")
+                .MaximumLength(100).WithMessage("O nome do tipo de embalegem deve ter no máximo 100 caracteres");
+        }
+    }
+}
