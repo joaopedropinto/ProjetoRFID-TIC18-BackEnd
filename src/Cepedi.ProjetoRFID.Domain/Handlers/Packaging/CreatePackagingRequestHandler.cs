3ce955ab2221c09c0bb1537ef2b1fb5eb344b484
Victor Quirino <victorsouzaq@gmail.com>
2024-10-15 15:44:22 -0300

    [#87]-Correction in the registration endpoint and package update to prevent duplicate names

--- src/Cepedi.ProjetoRFID.Domain/Handlers/Packaging/CreatePackagingRequestHandler.cs
+++ src/Cepedi.ProjetoRFID.Domain/Handlers/Packaging/CreatePackagingRequestHandler.cs
@@ -1,7 +1,11 @@
-﻿using Cepedi.ProjetoRFID.Domain.Entities;
+﻿using System.Globalization;
+using Cepedi.ProjetoRFID.Domain.Entities;
 using Cepedi.ProjetoRFID.Domain.Repositories;
+using Cepedi.ProjetoRFID.Shared.Enums;
+using Cepedi.ProjetoRFID.Shared.Exceptions;
 using Cepedi.ProjetoRFID.Shared.Requests.Packaging;
 using Cepedi.ProjetoRFID.Shared.Responses.Packaging;
+using Cepedi.ProjetoRFID.Shared.Utils;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
@@ -21,10 +25,17 @@ namespace Cepedi.ProjetoRFID.Domain.Handlers.Packaging
 
         public async Task<Result<CreatePackagingResponse>> Handle(CreatePackagingRequest request, CancellationToken cancellationToken)
         {
+            var existingPackaging = await _packagingRepository.ReturnActivePackagingByNameAsync(request.Name);
+
+            if(existingPackaging is not null) 
+            {
+                return Result.Error<CreatePackagingResponse>(new ExceptionApplication(RegisteredErrors.PackageNameAlreadyExists));
+            }
+            
             var packaging = new PackagingEntity()
             {
-                Name = request.Name
-            };
+                Name = char.ToUpper(request.Name[0]) + request.Name.Substring(1).ToLower()
+			};
 
             await _packagingRepository.CreatePackagingAsync(packaging);
 
